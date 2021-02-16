f1b3a087af3ceaf56c4c4fe66153b1628843e606
Preston <pres.10.nap@gmail.com>
2021-02-16 07:26:29 -0500

    started, beginning with about page

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header'
+import About from './components/about'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <About />
+        </div>
+    );
+}
+
+export default App;
---
+++ components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <nav id="nav-wrap" style="display: block;">
+
+                <ul class="navBar">
+                    <li><a>Home</a></li>
+                    <li><a>Home2</a></li>
+                    <li><a>Home3</a></li>
+                    <li><a>Home4</a></li>
+                    <li><a>Home5</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
